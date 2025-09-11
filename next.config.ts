8b3726a4a799abac6bd2fd40d05f2ceecd56e65c
coffeewithdonut <donut@purebase.co>
2025-09-11 13:22:57 +1000

    testing this

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
 import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.externals = {
+      ["@solana/web3.js"]: "commonjs @solana/web3.js",
+      ["@solana/spl-token"]: "commonjs @solana/spl-token",
+    }
+    return config
+  },
+}
 
 export default nextConfig
